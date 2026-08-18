75eaafd1bd88cb7c3c48f51c066731576c062d7a
mo670 <83023180+mo670@users.noreply.github.com>
2026-08-19 01:54:39 +0600

    Fix Monolog log path and stream handler for Vercel read-only filesystem

--- api/index.php
+++ api/index.php
@@ -6,6 +6,13 @@ $tmpStorage = '/tmp/storage';
 @mkdir($tmpStorage . '/framework/sessions', 0755, true);
 @mkdir($tmpStorage . '/framework/cache', 0755, true);
 @mkdir($tmpStorage . '/bootstrap/cache', 0755, true);
+@mkdir($tmpStorage . '/logs', 0755, true);
+
+$tmpLogFile = $tmpStorage . '/logs/laravel.log';
+if (!file_exists($tmpLogFile)) {
+    @touch($tmpLogFile);
+    @chmod($tmpLogFile, 0666);
+}
 
 // 2. Prepare SQLite database in /tmp
 $dbPath = '/tmp/database.sqlite';
@@ -24,12 +31,19 @@ $_ENV['STORAGE_PATH'] = $tmpStorage;
 $_ENV['VIEW_COMPILED_PATH'] = $tmpStorage . '/framework/views';
 $_ENV['DB_CONNECTION'] = 'sqlite';
 $_ENV['DB_DATABASE'] = $dbPath;
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['LOG_STACK'] = 'stderr';
+$_ENV['LOG_PATH'] = $tmpLogFile;
 
 putenv('STORAGE_PATH=' . $tmpStorage);
 putenv('VIEW_COMPILED_PATH=' . $tmpStorage . '/framework/views');
 putenv('DB_CONNECTION=sqlite');
 putenv('DB_DATABASE=' . $dbPath);
+putenv('LOG_CHANNEL=stderr');
+putenv('LOG_STACK=stderr');
+putenv('LOG_PATH=' . $tmpLogFile);
 
 // 4. Forward request to public/index.php
 require __DIR__ . '/../public/index.php';
+
 
