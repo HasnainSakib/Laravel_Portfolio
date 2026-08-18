b4767b45992ba38033a18a6cad3633d97a6e38a0
mo670 <83023180+mo670@users.noreply.github.com>
2026-08-19 02:11:07 +0600

    Use withStoragePath in ApplicationBuilder before boot and default LOG_CHANNEL to stderr

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,11 +15,8 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->withStoragePath(file_exists('/tmp') ? '/tmp/storage' : dirname(__DIR__).'/storage')
+    ->create();
 
-if (file_exists('/tmp')) {
-    $app->useStoragePath('/tmp/storage');
-}
-
-return $app;
 
